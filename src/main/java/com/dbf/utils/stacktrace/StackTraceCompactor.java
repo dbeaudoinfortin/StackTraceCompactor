550645cc05c902c1c8ef2fec508d177a0ed8c5a3
dbeaudoinfortin <david.fortin@8x8.com>
2024-07-06 17:57:22 -0400

    Improved the compaction of Class names containing anonymous inner classes. Further shortened the way line numbers are displayed. Removed duplicated exception messages when an exception is wrapped multiple times. Added new unit tests.

--- src/main/java/com/dbf/utils/stacktrace/StackTraceCompactor.java
+++ src/main/java/com/dbf/utils/stacktrace/StackTraceCompactor.java
@@ -13,9 +13,6 @@ public class StackTraceCompactor {
 	 */
 	public static String getCompactStackTrace(Throwable t) {
 		StringBuilder sb = new StringBuilder(200);
-		
-		sb.append("\nStackTrace:\n");
-		
 		//Apache Commons produces a more compact stack trace that clearly indicates [wrapped] lines
 		String[] stackElements = ExceptionUtils.getRootCauseStackTrace(t);
 		collapseStackElements(stackElements);
@@ -34,92 +31,133 @@ public class StackTraceCompactor {
 	 * @return void
 	 */
 	public static void collapseStackElements(String[] stackElements) {
-		String previousValidPackageName = "";
-		StringBuilder sb = new StringBuilder(200); //Reuse the string builder
+		if (stackElements.length < 2) return;
 		
-		for(int i = 0; i < stackElements.length; i++) {
+		String previousValidPackageName = "";
+		String previousExceptionMessage = stackElements[0];
+		
+		StringBuilder sb = new StringBuilder(200); //Reuse the string builder
+		for(int i = 1; i < stackElements.length; i++) {
 			sb.setLength(0);
 			String line = stackElements[i];
 				
 			//Validate - Check that the line starts looks like a regular stack trace line
 			//eg:    at okhttp3.internal.connection.RealConnection.connect(RealConnection.java:149)
 			int atIndex = line.indexOf("at ");
-			if(atIndex < 0) continue;
+			if(atIndex > -1) {
+				int openBracketIndex = line.lastIndexOf('(');
+				if(openBracketIndex < 0) continue;
+				
+				int methodIndex = line.lastIndexOf('.', openBracketIndex);
+				if(methodIndex < 0) continue;
+				
+				int closeBracketIndex = line.lastIndexOf(')');
+				if(closeBracketIndex < 0 || closeBracketIndex <= openBracketIndex) continue;
+				
+				//Extract the package name
+				String packageName = line.substring(atIndex + 3, methodIndex);
+				
+				//Determine the class name
+				//Note: could also be in the form of 'at RealConnection.connect(RealConnection.java:149)'
+				int classIndex = line.lastIndexOf('.', methodIndex - 1);
+				String className = (classIndex < 0) ? packageName : line.substring(classIndex + 1, methodIndex);
+				
+				//ClassName might contain an inner class
+				//eg: at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)
+				int innerClassIndex = className.indexOf("$");
+				if (innerClassIndex > 0) className = className.substring(0, innerClassIndex);
+				
+				//Determine the file name
+				//Note: could also be in the form of 'at okhttp3.internal.connection.RealConnection.connect(RealConnection.java)'
+				//or 'at okhttp3.internal.connection.RealConnection.connect(RealConnection)'
+				//or 'at java.net.PlainSocketImpl.socketConnect(Native Method)'
+				int colonIndex = line.indexOf(':', openBracketIndex);
+				int dotIndex = line.indexOf('.', openBracketIndex);
+				int fileEndIndex = (colonIndex < 0) ? closeBracketIndex : colonIndex;
+				String fileName = line.substring(openBracketIndex + 1, (dotIndex < 0) ? fileEndIndex : dotIndex);
+				
+				//Build the new line
+				sb.append(line.substring(0, atIndex + 3)); //"    at "
+				
+				//Determine package overlap with the previous package name
+				sb.append(overlapPackageName(previousValidPackageName, packageName));
 			
-			int openBracketIndex = line.lastIndexOf('(');
-			if(openBracketIndex < 0) continue;
-			
-			int methodIndex = line.lastIndexOf('.', openBracketIndex);
-			if(methodIndex < 0) continue;
-			
-			int closeBracketIndex = line.lastIndexOf(')');
-			if(closeBracketIndex < 0 || closeBracketIndex <= openBracketIndex) continue;
-			
-			//Extract the package name
-			String packageName = line.substring(atIndex + 3, methodIndex);
-			
-			//Determine the class name
-			//Note: could also be in the form of 'at RealConnection.connect(RealConnection.java:149)'
-			int classIndex = line.lastIndexOf('.', methodIndex - 1);
-			String className = (classIndex < 0) ? packageName : line.substring(classIndex + 1, methodIndex);
-			
-			//Determine the file name
-			//Note: could also be in the form of 'at okhttp3.internal.connection.RealConnection.connect(RealConnection.java)'
-			//or 'at okhttp3.internal.connection.RealConnection.connect(RealConnection)'
-			//or 'at java.net.PlainSocketImpl.socketConnect(Native Method)'
-			int colonIndex = line.indexOf(':', openBracketIndex);
-			int dotIndex = line.indexOf('.', openBracketIndex);
-			int fileEndIndex = (colonIndex < 0) ? closeBracketIndex : colonIndex;
-			String fileName = line.substring(openBracketIndex + 1, (dotIndex < 0) ? fileEndIndex : dotIndex);
-			String fileNameWithExtension = line.substring(openBracketIndex + 1, fileEndIndex);
-			
-			//Build the new line
-			sb.append(line.substring(0, atIndex + 3)); //"    at "
-			
-			//Determine package overlap with the previous package name
-			sb.append(overlapPackageName(previousValidPackageName, packageName));
-		
-			sb.append(line.substring(methodIndex, openBracketIndex + 1));
-			
-			if(fileName.equals(className)) {
-				sb.append("~");
-				//Check if it Follows standard java convention for naming
-				if(dotIndex > -1 && !fileNameWithExtension.endsWith(".java")) {
-					sb.append(line.substring(dotIndex, fileEndIndex));
+				sb.append(line.substring(methodIndex, openBracketIndex + 1));
+				
+				if(fileName.equals(className)) {
+					//Check if it Follows standard java convention for naming ("ParentRunner.java")
+					if(dotIndex > -1 && !line.substring(openBracketIndex + 1, fileEndIndex).endsWith(".java")) {
+						//Replace just the first part and keep the extension
+						//eg. "(ParentRunner.crazy:56)" -> "(~.crazy:56)"
+						sb.append("~");
+						sb.append(line.substring(dotIndex, closeBracketIndex + 1));
+					} else {
+						//Retain just the line number, if present. Retain nothing otherwise.
+						if(colonIndex > 0) {
+							//"(ParentRunner.java:56)" -> "(56)"
+							sb.append(line.substring(colonIndex +1 , closeBracketIndex + 1));
+						}else {
+							//eg. "(ParentRunner.java)" -> "()"
+							sb.append(")");
+						}
+					}
+				} else {
+					//Keep everything in the bracket as-is
+					//eg. (ParentRunner.java:66)
+					sb.append(line.substring(openBracketIndex + 1, closeBracketIndex + 1));
 				}
+						
+				stackElements[i] = sb.toString();
+				previousValidPackageName = packageName;
 			} else {
-				sb.append(fileNameWithExtension);
-			}
-			sb.append(line.substring(fileEndIndex, closeBracketIndex + 1));
-					
-			stackElements[i] = sb.toString();
-			previousValidPackageName = packageName;
+				//This is likely a "wrapped" line
+				//eg:    [wrapped] java.lang.RuntimeException: java.lang.IllegalArgumentException: Bad file provided.
+				int wrappedIndex = line.indexOf("[wrapped] ");
+				if(wrappedIndex < 0) continue;
+				
+				stackElements[i] = line.replace(previousExceptionMessage, "").trim(); //This will intentionally remove the leading space
+				previousExceptionMessage = line.substring(wrappedIndex + "[wrapped] ".length());
+			};
 		}
 	}
 	
 	private static String overlapPackageName(String basePackageName, String overlapPackageName) {
+		return overlapPackageName(basePackageName, overlapPackageName, '.');
+	}
+	private static String overlapPackageName(String basePackageName, String overlapPackageName, char packageSeperator) {
 		if(basePackageName.isEmpty())
     		return overlapPackageName;
 		
-		String[] baseSplits = StringUtils.split(basePackageName, '.');
-		String[] overlapSplits = StringUtils.split(overlapPackageName, '.');
+		String[] baseSplits = StringUtils.split(basePackageName, packageSeperator);
+		String[] overlapSplits = StringUtils.split(overlapPackageName, packageSeperator);
 		
 		StringBuilder sb = new StringBuilder(overlapPackageName.length());
 		String seperator = "";
 		boolean matchComplete = false;
 		
 		for(int i = 0; i < overlapSplits.length; i++) {
-			String segment = overlapSplits[i];
+			final String segment = overlapSplits[i];
 			sb.append(seperator);
-			if(matchComplete || i > baseSplits.length -1 || !segment.equals(baseSplits[i])) {
-				//If this is an extra segment, or the segments don't match, or matching has completed, then append it as-is
+			if(matchComplete || i > baseSplits.length -1 ) {
+				//If this is an extra segment, or matching has completed, then append it as-is
 				sb.append(segment);
 				matchComplete = true;
-			} else {
+			} else if (!segment.equals(baseSplits[i])) { 
+				//The segments don't match, check for inner class
+				//eg. "ParentRunner$2" vs. "ParentRunner$4" vs. "ParentRunner"
+				if(segment.indexOf('$') > 0 || baseSplits[i].indexOf('$') > 0) {
+					final String newSegment = overlapPackageName(baseSplits[i], segment, '$');
+					sb.append(newSegment);
+					matchComplete = segment.equals(newSegment);
+				} else {
+					sb.append(segment);
+					matchComplete = true;
+				}
+			}else {
 				//This segment is the same. Condense it
 				sb.append(segment.charAt(0));
 			}
-			seperator = ".";
+			seperator = "" + packageSeperator;
 		}
 		return sb.toString();
 	}
--- src/test/java/com/dbf/utils/stacktrace/StackTraceCompactorTest.java
+++ src/test/java/com/dbf/utils/stacktrace/StackTraceCompactorTest.java
@@ -1,5 +1,6 @@
 package com.dbf.utils.stacktrace;
 
+import java.io.IOException;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,13 +9,12 @@ import org.junit.Test;
 public class StackTraceCompactorTest {
 
 	@Test
-	public void testStackElementCompaction()
-	{
+	public void testStackElementCompaction() {
 		String rawStack = "java.net.ConnectException: Connection refused (Connection refused)\r\n" + 
 				"    at AbstractPlainSocketImpl.doConnect(AbstractPlainSocketImpl.java:350)\r\n" + 
 				"    at java.net.PlainSocketImpl.socketConnect(Native Method)\r\n" + 
 				"    at java.net.AbstractPlainSocketImpl.doConnect(AbstractPlainSocketImpl.java:350)\r\n" + 
-				"    at java.net.AbstractPlainSocketImpl.connectToAddress(AbstractPlainSocketImpl.java:206)\r\n" + 
+				"    at java.net.AbstractPlainSocketImpl.connectToAddress(AbstractPlainSocketImpl.blorg)\r\n" + 
 				"    at java.net.AbstractPlainSocketImpl.connect(AbstractPlainSocketImpl.blorg:188)\r\n" + 
 				"    at java.net.SocksSocketImpl.connect(SocksSocketImpl.java:392)\r\n" + 
 				"    at java.net.Socket.connect(Socket.java:589)\r\n" + 
@@ -34,38 +34,127 @@ public class StackTraceCompactorTest {
 				"    at okhttp3.internal.http.RealInterceptorChain.proceed(RealInterceptorChain.java:67)\r\n" + 
 				"    at okhttp3.internal.http.BridgeInterceptor.intercept(BridgeInterceptor.java:93)\r\n" + 
 				"    at RealInterceptorChain.proceed(RealInterceptorChain.java:92)\r\n" + 
-				"at okhttp3.internal.http.RetryAndFollowUpInterceptor.intercept(RetryAndFollowUpInter�";
+				"at okhttp3.internal.http.RetryAndFollowUpInterceptor.intercept(RetryAndFollowUpInter�"; //The end character in invalid on purpose
 		
 		String expectedStack = "java.net.ConnectException: Connection refused (Connection refused)\r\n" + 
-				"    at AbstractPlainSocketImpl.doConnect(~:350)\r\n" + 
+				"    at AbstractPlainSocketImpl.doConnect(350)\r\n" + 
 				"    at java.net.PlainSocketImpl.socketConnect(Native Method)\r\n" + 
-				"    at j.n.AbstractPlainSocketImpl.doConnect(~:350)\r\n" + 
-				"    at j.n.A.connectToAddress(~:206)\r\n" + 
+				"    at j.n.AbstractPlainSocketImpl.doConnect(350)\r\n" + 
+				"    at j.n.A.connectToAddress(~.blorg)\r\n" + 
 				"    at j.n.A.connect(~.blorg:188)\r\n" + 
-				"    at j.n.SocksSocketImpl.connect(~:392)\r\n" + 
-				"    at j.n.Socket.connect(~:589)\r\n" + 
-				"    at okhttp3.internal.platform.Platform.connectSocket(~:124)\r\n" + 
-				"    at o.i.connection.RealConnection.connectSocket(~:223)\r\n" + 
-				" [wrapped] java.net.ConnectException: Failed to connect to crazy-broken-server/192.168.0.0:1001\r\n" + 
-				"    at o.i.c.R.connectSocket(~:225)\r\n" + 
-				"    at o.i.c.R.connect(~:149)\r\n" + 
-				"    at o.i.c.StreamAllocation.findConnection(~:192)\r\n" + 
-				"    at o.i.c.S.findHealthyConnection(~:121)\r\n" + 
-				"    at o.i.c.S.newStream(~:100)\r\n" + 
-				"    at o.i.c.ConnectInterceptor.intercept(~:42)\r\n" + 
-				"    at o.i.http.RealInterceptorChain.proceed(~)\r\n" + 
-				"    at o.i.h.R.proceed(~:67)\r\n" + 
+				"    at j.n.SocksSocketImpl.connect(392)\r\n" + 
+				"    at j.n.Socket.connect(589)\r\n" + 
+				"    at okhttp3.internal.platform.Platform.connectSocket(124)\r\n" + 
+				"    at o.i.connection.RealConnection.connectSocket(223)\r\n" + 
+				"[wrapped] java.net.ConnectException: Failed to connect to crazy-broken-server/192.168.0.0:1001\r\n" + 
+				"    at o.i.c.R.connectSocket(225)\r\n" + 
+				"    at o.i.c.R.connect(149)\r\n" + 
+				"    at o.i.c.StreamAllocation.findConnection(192)\r\n" + 
+				"    at o.i.c.S.findHealthyConnection(121)\r\n" + 
+				"    at o.i.c.S.newStream(100)\r\n" + 
+				"    at o.i.c.ConnectInterceptor.intercept(42)\r\n" + 
+				"    at o.i.http.RealInterceptorChain.proceed()\r\n" + 
+				"    at o.i.h.R.proceed(67)\r\n" + 
 				"    at o.i.cache.CacheInterceptors.intercept(CacheInterceptor.java:93)\r\n" + 
-				"    at o.i.http.RealInterceptorChain.proceed(~:92)\r\n" + 
-				"    at o.i.h.R.proceed(~:67)\r\n" + 
-				"    at o.i.h.BridgeInterceptor.intercept(~:93)\r\n" + 
-				"    at RealInterceptorChain.proceed(~:92)\r\n" + 
-				"at okhttp3.internal.http.RetryAndFollowUpInterceptor.intercept(RetryAndFollowUpInter�";
+				"    at o.i.http.RealInterceptorChain.proceed(92)\r\n" + 
+				"    at o.i.h.R.proceed(67)\r\n" + 
+				"    at o.i.h.BridgeInterceptor.intercept(93)\r\n" + 
+				"    at RealInterceptorChain.proceed(92)\r\n" + 
+				"at okhttp3.internal.http.RetryAndFollowUpInterceptor.intercept(RetryAndFollowUpInter�"; //The end character in invalid on purpose
 		
 		//Split the stack trace into elements, compact it, and then recombine it
 		String[] originalStackElements = StringUtils.split(rawStack, "\r\n");
 		StackTraceCompactor.collapseStackElements(originalStackElements);
-		Assert.assertEquals(combineStackElements(originalStackElements), expectedStack);
+		Assert.assertEquals(expectedStack, combineStackElements(originalStackElements));
+	}
+	
+	@Test
+	public void tripleWrappedMatchingExcpetionTest() {
+		Throwable testThrowable = null;
+		try {
+			try {
+				throw new IOException("Your pipe is broken, sir.");
+			} catch (Throwable t) {
+				try {
+					throw new RuntimeException(t);
+				} catch (Throwable t2) {
+					throw new RuntimeException(t2);
+				}
+			}
+		} catch (Throwable t) {
+			testThrowable = t;
+		}
+		String resultingStack = StackTraceCompactor.getCompactStackTrace(testThrowable);
+		
+		//Verify that redundant exception messages have been removed
+		Assert.assertTrue(resultingStack.contains("java.io.IOException: Your pipe is broken, sir." + System.lineSeparator()));
+		Assert.assertTrue(resultingStack.contains("[wrapped] java.lang.RuntimeException:" + System.lineSeparator()));
+		Assert.assertEquals(StringUtils.countMatches(resultingStack, "[wrapped] java.lang.RuntimeException:" + System.lineSeparator()), 2);
+	}
+	
+	@Test
+	public void tripleWrappedExcpetionTest() {
+		Throwable testThrowable = null;
+		try {
+			try {
+				throw new IOException("Your pipe is broken, sir.");
+			} catch (Throwable t) {
+				try {
+					throw new IllegalArgumentException(t);
+				} catch (Throwable t2) {
+					throw new RuntimeException(t2);
+				}
+			}
+		} catch (Throwable t) {
+			testThrowable = t;
+		}
+		String resultingStack = StackTraceCompactor.getCompactStackTrace(testThrowable);
+		
+		//Verify that redundant exception messages have been removed
+		Assert.assertTrue(resultingStack.contains("java.io.IOException: Your pipe is broken, sir." + System.lineSeparator()));
+		Assert.assertTrue(resultingStack.contains("[wrapped] java.lang.IllegalArgumentException:" + System.lineSeparator()));
+		Assert.assertTrue(resultingStack.contains("[wrapped] java.lang.RuntimeException:" + System.lineSeparator()));
+	}
+	
+	@Test
+	public void doubleWrappedExcpetionTest() {
+		Throwable testThrowable = null;
+		try {
+			try {
+				throw new IOException("Your pipe is broken, sir.");
+			} catch (Throwable t) {
+				try {
+					throw new IllegalArgumentException("Bad file provided.", t);
+				} catch (Throwable t2) {
+					throw new RuntimeException(t2);
+				}
+			}
+		} catch (Throwable t) {
+			testThrowable = t;
+		}
+		String resultingStack = StackTraceCompactor.getCompactStackTrace(testThrowable);
+		
+		//Verify that redundant exception messages have been removed
+		Assert.assertTrue(resultingStack.contains("java.io.IOException: Your pipe is broken, sir." + System.lineSeparator()));
+		Assert.assertTrue(resultingStack.contains("[wrapped] java.lang.IllegalArgumentException: Bad file provided." + System.lineSeparator()));
+		Assert.assertTrue(resultingStack.contains("[wrapped] java.lang.RuntimeException:" + System.lineSeparator()));
+	}
+	
+	@Test
+	public void singleWrappedExcpetionTest() {
+		
+		//Can't match the entire stack because it will change everytime a line of code is changed in this file
+		String expectedStack = "java.io.IOException: Your pipe is broken, sir." + System.lineSeparator()
+				+ "	at com.dbf.utils.stacktrace.StackTraceCompactorTest.singleWrappedExcpetionTest(";
+		
+		Throwable testThrowable = null;
+		try {
+			throw new IOException("Your pipe is broken, sir.");
+		} catch (Throwable t) {
+			testThrowable = t;
+		}
+		String resultingStack = StackTraceCompactor.getCompactStackTrace(testThrowable);	
+		Assert.assertTrue(resultingStack.startsWith(expectedStack));
 	}
 	
 	private static String combineStackElements(String[] stackElements) {
